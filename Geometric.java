ad50bbe2c59d4ec7e0df6c73ae2375826392b064
Zeyad_Gasser <141816908+ZEYAD-GASSER@users.noreply.github.com>
2024-07-29 16:18:37 +0300

    Create Geometric.java

---
+++ Geometric.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.geometric_obj;
+public abstract class Geometric_obj {
+     public abstract double getArea(double radius_length);
+
+    public abstract double getPerimeter(double radius_length);
+
+    public abstract double getArea(double length, double width);
+
+    public abstract double getPerimeter(double length, double width);
+
+    public static void main(String[] args) {
+         Window ob1 = new Window();
+    }
+}
